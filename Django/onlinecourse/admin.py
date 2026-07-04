852c5faee2eb208903ee960e53861dba6663e5fd
ahmedfawzyjr <ahmedfawzyjr@users.noreply.github.com>
2026-07-04 12:44:14 +0300

    Add Django project files

---
+++ Django/onlinecourse/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    inlines = [QuestionInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'lesson', 'grade']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+
